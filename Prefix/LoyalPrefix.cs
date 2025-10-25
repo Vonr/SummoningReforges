2f4ff00bdf0c9758b147461132748c00b6c4ddb8
Qther <qther@tuta.io>
2025-10-25 20:32:56 +0800

    feat: rebalance Loyal

--- Prefix/LoyalPrefix.cs
+++ Prefix/LoyalPrefix.cs
@@ -5,11 +5,11 @@ namespace SummoningReforges.Prefix
         public override StaffType StaffType => StaffType.Minion;
 
         public override float Value => 1.6f;
-        public override float TagDamage => 4f / 3f;
+        public override float TagDamage => 1.5f;
 
         public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
         {
-            damageMult *= 0.85f;
+            damageMult *= 0.8f;
         }
     }
 }
